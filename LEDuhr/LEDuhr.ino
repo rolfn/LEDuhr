f31051b253fcc4a6543a83f593e7408f048475e7
Rolf Niepraschk <Rolf.Niepraschk@gmx.de>
2015-12-06 20:57:21 +0100

    ...

--- LEDuhr/LEDuhr.ino
+++ LEDuhr/LEDuhr.ino
@@ -77,8 +77,8 @@ void setup() {
     button.attachClick(click);
     button.attachDoubleClick(doubleclick);
     button.attachLongPressStart(longPressStart);
-    button.attachLongPressStop(longPressStop);
-    button.attachDuringLongPress(longPress);
+    //button.attachLongPressStop(longPressStop);
+    //button.attachDuringLongPress(longPress);
 
     DISP1.begin(0x00);
     DISP1.setPoint(COLON);
